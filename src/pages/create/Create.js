c3bdd98722bf7636d389b66a57256d2ed96f6ed7
Ollie Pegram <olliepegram@gmail.com>
2021-11-11 11:12:58 +1100

    search implemented

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
 
 import './Create.css';
@@ -10,6 +11,7 @@ export default function Create() {
     const [newIngredient, setNewIngredient] = useState('');
     const [ingredients, setIngredients] = useState([]);
     const ingredientInput = useRef(null);
+    const history = useNavigate();
     const { postData, data, error } = useFetch(
         'http://localhost:3000/recipes',
         'POST'
@@ -36,6 +38,12 @@ export default function Create() {
         setNewIngredient('');
         ingredientInput.current.focus();
     };
+
+    useEffect(() => {
+        if (data) {
+            history('/');
+        }
+    }, [data, history]);
 
     return (
         <div className='create'>
